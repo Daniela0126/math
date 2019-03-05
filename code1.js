ec03064d9cf55138e686f6f494801acc85c2bde6
Daniela Valencia <daniela.valenica@gmail.com>
2019-03-05 17:05:26 +0000

    second day

---
+++ code1.js
@@ -0,0 +1,31 @@
+// JavaScript File
+
+$('document').ready(function(){
+    var userInput = "";
+    var parts = [];
+    var encrypt = [
+        
+    {
+        plaintext: "a"
+        cipher: ".-"
+    }{
+        plaintext: "b"
+        cipher: "-..."
+    }{
+        plaintext: "c"
+        cipher: "-.-."
+    }{
+        plaintext: "d"
+        cipher: "-.."
+    }{
+        plaintext "e"
+        chiper: "."
+    }{
+        plaintext: "f"
+        cipher: ""
+    }
+         
+        
+        ];
+        
+});
